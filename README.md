41a60e2ea5fa811675f4bab9745535f125ba1b4f
卞天灵 <bian_tianling@icloud.com>
2021-08-11 16:46:08 +0800

    setup & readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Prod Tools
+
+## Motivation
+
+As I progress with my work, I discovered that I have been writing some scripts over and over. This
+is a waste of time, and also not smart. I decide to organize these scripts & functions that I use
+repeatedly with this repo.
+
+scripts may contain
+  - hdfs tools
+  - some itertools that python doesn't ship by default
+  - others
+
+Noted that this repo cannot contain anything related to business secrets.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Collection of productivity scripts at work'
+LONG_DESCRIPTION = long_description
+
+# Setting up
+setup(
+    name="prodtools",
+    version=VERSION,
+    author="Tianling Bian",
+    author_email="bian_tianling@icloud.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'prod-tools'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
